69fc9d11971c671f394bbdf93c7bd3abd89c2c65
Relsi Maron <relsi@codeup.com.br>
2020-12-29 22:30:42 -0300

    add a function to list loctions on map

--- locations/static/map.js
+++ locations/static/map.js
@@ -1,4 +1,7 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const map = L.map('map')
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
-map.fitWorld();
+const locations = JSON.parse(document.getElementById('locations-data').textContent);
+let feature = L.geoJSON(locations).bindPopup(function (layer) { return layer.feature.properties.place; }).addTo(map);
+map.fitBounds(feature.getBounds(), { padding: [100, 100] });
+
--- locations/views.py
+++ locations/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+import json
+
+from django.core.serializers import serialize
 from django.views.generic.base import TemplateView
+
+from .models import Location
 
 class LocationsMapView(TemplateView):
     template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        """Return the view context data."""
+        location = super().get_context_data(**kwargs)
+        location["locations"] = json.loads(serialize("geojson", Location.objects.all()))
+        return location
